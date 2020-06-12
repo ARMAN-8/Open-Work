c3aebee8aed770f09c0e15b33bb0c080d5bbc65a
Arman <galamat.aa@gmail.com>
2020-06-12 15:03:52 +0600

    дщкуь

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    // SideNav Button Initialization
+    $(".button-collapse").sideNav();
+    // SideNav Scrollbar Initialization
+    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    var ps = new PerfectScrollbar(sideNavScrollbar);
+});
+
+// Tooltips Initialization
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
